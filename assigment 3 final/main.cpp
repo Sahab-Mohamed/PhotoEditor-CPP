313db9662bd3beb3f5edf8dafd0ade04a575f15f
rana ayman <rana486999@gmail.com>
2022-04-06 16:09:18 +0200

    Initial Commit

---
+++ assigment 3 final/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  assigment 3 final
+//
+//  Created by Rana ayman on 06/04/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
