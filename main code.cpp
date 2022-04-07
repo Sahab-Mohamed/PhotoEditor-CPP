5435846a0eb849d48457131670c750fc1a245d9a
Rana486 <100386390+Rana486@users.noreply.github.com>
2022-04-07 18:23:55 +0200

    Update main code.cpp

--- main code.cpp
+++ main code.cpp
@@ -10,7 +10,8 @@ using namespace std;
    cin >> imageFileName;
    strcat (imageFileName, ".bmp");
    readGSBMP(imageFileName, image);}*/
-
+// void bW_filter ()
+//void saveImage ();
 int main() {
     int choice;
     cout<< "hello user, ";
@@ -26,61 +27,66 @@ int main() {
     cin >> choice;
     cin.ignore();
     if (choice == 1){
-        // black and whiter filter
+        //----------------- black and white filter--------------
+       /* loadImage();
+        bW_filter();
+        saveImage();
+        return 0; */
+        
     }
     
         else if (choice == 2){
-       // invert filter
+       //-------------------- invert filter----------
            
        }
         else if (choice == 3 ){
-            // merge filter
+            //------------------- merge filter-----------
             
             cout << "please enter name of image file to merge with: " ;
             
         }
         else if (choice == 4){
     int flip_choice;
-            // flip image
+            // ----------------flip image--------
             cout << " 1. flip horizontaly or 2. vertically";
             cin >> flip_choice;
             cin.ignore();
             if (flip_choice== 1){
-                // flip horizontaly
+                // ---------------flip horizontaly
             }
             else if (flip_choice == 2){
-                // flip vertically
+                // ------------flip vertically
             
         }
 }
         else if (choice == 5){
             int light_choice;
-            // darken and lighten filter
+            //------------------ darken and lighten filter
             cout<< "do you want to 1. lighten or 2 . darken";
             cin >> light_choice;
             cin.ignore();
             if (light_choice==1){
-                // lighten
+                // ---------lighten
             }
             else if (light_choice==2){
-                 // darken
+                 // -----------darken
                     
                 }
             }
         else if (choice == 6){
-            // rotate image
+            // ------------------rotate image--------
             int rotate_choice;
             cout << "rotate 1. (90) \n 2. (180) \n 3. (360) degree";
             cin >> rotate_choice;
             cin.ignore();
             if (rotate_choice==1){
-                // rotate 90 degree
+                // ----------rotate 90 degree
             }
             else if (rotate_choice==2){
-                 // rotate 180 degree
+                 //------------ rotate 180 degree
                 }
             else if (rotate_choice == 3) {
-                //rotate  360 degree
+                //--------rotate  360 degree
             }
         }
 }
@@ -92,3 +98,19 @@ int main() {
    readGSBMP(imageFileName, image);
 }
 */
+/*void bW_filter ()
+ for (int i = 0; i < SIZE; i++) {
+   for (int j = 0; j< SIZE; j++) {
+ if (image[i][j] > 127)
+     image[i][j] = 255;
+ else
+     image[i][j] = 0;
+*/
+/*void saveImage () {
+   char imageFileName[100];
+   cout << "Enter the target image file name: ";
+   cin >> imageFileName;
+   strcat (imageFileName, ".bmp");
+   writeGSBMP(imageFileName, image);
+}
+ */
