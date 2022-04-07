209591ff37ba9ede879e093856f1d3f6db16e48b
Rana486 <100386390+Rana486@users.noreply.github.com>
2022-04-07 21:07:34 +0200

    Update main code.cpp

--- main code.cpp
+++ main code.cpp
@@ -2,18 +2,18 @@
 #include <fstream>
 #include <cstring>
 #include <cmath>
-#include "bmplib.cpp"
+#include "cmake-build-debug/bmplib.cpp"
 using namespace std;
 unsigned char image[SIZE][SIZE];
-
+void rotate_image_90();
+void rotate_image_180();
+void rotate_image_270();
 void loadImage ();
 void saveImage ();
 void bW_filter ();
 int main() {
     int choice;
     cout<< "hello user, ";
-    cout << " please enter name of image to process"<<endl;
-     loadImage();
     cout  << "what filter would you like to do?\n"
           << "1. Black &white filter " << endl
           <<"2. invert filter" << endl
@@ -73,88 +73,32 @@ int main() {
     else if (choice == 6){
         // ------------------rotate image--------
         int rotate_choice;
-        cout << "rotate 1. (90) \n 2. (180) \n 3. (360) degree";
+        cout << "rotate 1. (90) \n 2. (180) \n 3. (270) degree";
         cin >> rotate_choice;
         cin.ignore();
-        
-        
-        
-        
-        void doSomethingForImage() {
-    string userinput = "";
-    cout << "please choose if u want to rotate by 90 degrees or 180 or 270: ";
-    cin >> userinput;
- if (userinput == "90") {
-
-
-        //90
-
-        for (int i = 0; i < SIZE; i++) {
-
-            for (int j = i; j < SIZE; j++) {
-                int temp = image[i][j];
-                image[i][j] = image[j][i];
-                image[j][i] = temp;
-            }
-        }
-
-
-        for (int i = 0; i < SIZE; i++) {
-
-            for (int j = 0; j < SIZE / 2; j++) {
-                int temp = image[i][j];
-                image[i][j] = image[i][SIZE - 1 - j];
-                image[i][SIZE - 1 - j] = temp;
-            }
-        }
-    }
- else if (userinput == "180") {
-
-
-//180
-
-        for (int i = 0; i < SIZE / 2; ++i) {
-            for (int j = 0; j < SIZE; ++j) {
-                swap(image[i][j], image[SIZE - i - 1][SIZE - j - 1]);
-            }
-
-        }
-    }
- else if (userinput == "270") {
-
-
-//270
-
-        for (int i = 0; i < SIZE; ++i) {
-            for (int j = i; j < SIZE; ++j) {
-                swap(image[i][j], image[j][i]);
-
-            }
-        }
-        for (int i = 0; i < SIZE; i++) {
-
-            for (int j = 0; j < SIZE / 2; j++) {
-                swap(image[i][j], image[j][SIZE - 1 - j]);
-            }
-        }
-    }
- else {
-        cout << "invalid input";
-    }
-}
-        
-        
-        
-        
-        
-        if (rotate_choice==1){
+        if (rotate_choice == 1){
             // ----------rotate 90 degree
+            loadImage();
+            rotate_image_90();
+            saveImage();
+            return 0;
+
         }
+
         else if (rotate_choice==2){
             //------------ rotate 180 degree
+            loadImage();
+            rotate_image_180();
+            saveImage();
+            return 0;
+
         }
         else if (rotate_choice == 3) {
-            //--------rotate  360 degree
+            //--------rotate  270 degree
+            loadImage();
+            rotate_image_270();
+            saveImage();
+            return 0;
         }
     }
 }
@@ -167,20 +111,72 @@ int main() {
 }
 
  void bW_filter () {
+    long avg = 0;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+             avg += image[i][j];
+         }
+     }
+     avg /= (SIZE*SIZE);
      for (int i = 0; i < SIZE; i++) {
-         if (image[i][j] > 127)
-             image[i][j] = 255;
-         else
-             image[i][j] = 0;
+         for (int j = 0; j < SIZE; j++) {
+             if (image[i][j] > avg)
+                 image[i][j] = 255;
+             else
+                 image[i][j] = 0;
+         }
      }
  }
 
+     void saveImage() {
+         char imageFileName[100];
+         cout << "Enter the target image file name: ";
+         cin >> imageFileName;
+         strcat(imageFileName, ".bmp");
+         writeGSBMP(imageFileName, image);
+     }
+
+     void rotate_image_90() {
+         for (int i = 0; i < SIZE; i++) {
+
+             for (int j = i; j < SIZE; j++) {
+                 int temp = image[i][j];
+                 image[i][j] = image[j][i];
+                 image[j][i] = temp;
+             }
+         }
 
 
-void saveImage () {
-   char imageFileName[100];
-   cout << "Enter the target image file name: ";
-   cin >> imageFileName;
-   strcat (imageFileName, ".bmp");
-   writeGSBMP(imageFileName, image);
-}
+         for (int i = 0; i < SIZE; i++) {
+
+             for (int j = 0; j < SIZE / 2; j++) {
+                 int temp = image[i][j];
+                 image[i][j] = image[i][SIZE - 1 - j];
+                 image[i][SIZE - 1 - j] = temp;
+             }
+         }
+     }
+void rotate_image_180() {
+    for (int i = 0; i < SIZE / 2; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            swap(image[i][j], image[SIZE - i - 1][SIZE - j - 1]);
+        }}}
+void rotate_image_270() {
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = i; j < SIZE; ++j) {
+            swap(image[i][j], image[j][i]);
+
+        }
+    }
+    for (int i = 0; i < SIZE; i++) {
+
+        for (int j = 0; j < SIZE / 2; j++) {
+            swap(image[i][j], image[j][SIZE - 1 - j]);
+        }
+    }
+
+
+
+        }
+
+
